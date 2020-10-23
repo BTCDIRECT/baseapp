5f79d98a5db661fdea23580d93cd495c6e3980cb
oyershov <oyershov@heliostech.fr>
2020-10-23 17:00:15 +0200

    Fix: provide changes according to the code review

---
+++ src/modules/public/errorHandler/helpers/getMetaMaskErrorMessage.ts
@@ -0,0 +1,19 @@
+import { UnsupportedChainIdError } from '@web3-react/core';
+import {
+    NoEthereumProviderError,
+    UserRejectedRequestError as UserRejectedRequestErrorInjected,
+} from '@web3-react/injected-connector';
+
+export const getMetaMaskErrorMessage = (error: Error): string => {
+    if (error instanceof NoEthereumProviderError) {
+        return 'metamask.error.noExtension';
+    } else if (error instanceof UnsupportedChainIdError) {
+        return 'metamask.error.unsupportedNetwork';
+    } else if (error instanceof UserRejectedRequestErrorInjected) {
+        return 'metamask.error.unauthorized';
+    } else {
+        window.console.error(error);
+
+        return 'metamask.error.unknown';
+    }
+};
